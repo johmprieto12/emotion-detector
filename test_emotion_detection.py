cfb4d7aed9025b8b8d3ee72da8f62556b2111dba
Johm Prieto <johmprieto@gmail.com>
2025-09-05 21:30:26 -0500

    Update test_emotion_detection.py

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,23 +1,17 @@
-from EmotionDetection.emotion_detection import emotion_detector
 import unittest
-
-class TestEmotionDetection(unittest.TestCase):
+from EmotionDetection.emotion_detection import emotion_detector
+ 
+class TestEmotionDetector(unittest.TestCase):
     def test_emotion_detector(self):
-        # Note: These tests rely on the external API being available
-        result_1 = emotion_detector('I am glad this happened')
-        self.assertEqual(result_1['emotion'], 'joy')
+        result1 = emotion_detector(“I am glad this happened”)
+        self.assertEqual(result1['dominant_emotion'], 'joy')
+        result2 = emotion_detector(“I am really mad about this”)
+        self.assertEqual(result2['dominant_emotion'], 'anger')
+        result3 = emotion_detector(“I feel disgusted just hearing about this”)
+        self.assertEqual(result3['dominant_emotion'], 'disgust')
+        result4 = emotion_detector(“I am so sad about this”)
+        self.assertEqual(result4['dominant_emotion'], 'sadness')
+        result5 = emotion_detector(“I am really afraid that this will happen”)
+        self.assertEqual(result5['dominant_emotion'], 'fear')
 
-        result_2 = emotion_detector('I am really mad about this')
-        self.assertEqual(result_2['emotion'], 'anger')
-
-        result_3 = emotion_detector('I feel disgusted just hearing about this')
-        self.assertEqual(result_3['emotion'], 'disgust')
-
-        result_4 = emotion_detector('I am so sad about this')
-        self.assertEqual(result_4['emotion'], 'sadness')
-
-        result_5 = emotion_detector('I am really afraid that this will happen')
-        self.assertEqual(result_5['emotion'], 'fear')
-
-if __name__ == "__main__":
-    unittest.main()
+unittest.main()
