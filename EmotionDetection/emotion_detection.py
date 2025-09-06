1209cc0d35da8f9367831e4be143040e95de58d8
Johm Prieto <johmprieto@gmail.com>
2025-09-05 22:00:55 -0500

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,5 +1,4 @@
 import requests
-
 import json
 
 def emotion_detector(text_to_analyse):
@@ -8,8 +7,19 @@ def emotion_detector(text_to_analyse):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = {"raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers = headers)
-    res = json.loads(response.text)
-    formatted_response = res['emotionPredictions'][0]['emotion']
-    dominant_emotion = max(formatted_response, key = lambda x: formatted_response[x])
-    formatted_response['dominant_emotion'] = dominant_emotion
+    status_code = response.status_code
+    
+    if status_code == 400:
+        formatted_response = { 'anger': None,
+                             'disgust': None,
+                             'fear': None,
+                             'joy': None,
+                             'sadness': None,
+                             'dominant_emotion': None }
+    else:
+        res = json.loads(response.text)
+        formatted_response = res['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(formatted_response, key = lambda x: formatted_response[x])
+        formatted_response['dominant_emotion'] = dominant_emotion
+
     return formatted_response 
