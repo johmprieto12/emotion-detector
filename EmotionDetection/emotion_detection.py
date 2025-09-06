df3b14c9630c38188775dc57f5dc68caa023fcb4
Johm Prieto <johmprieto@gmail.com>
2025-09-05 21:59:08 -0500

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,13 +1,15 @@
 import requests
+
 import json
 
 def emotion_detector(text_to_analyse):
+
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    myobj = { "raw_document": { "text": text_to_analyse } }
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    response = requests.post(url, json=myobj, headers=header)
-    formatted_response = json.loads(response.text)
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
-    dominant_emotion = max(emotions, key=emotions.get)
-    score = emotions[dominant_emotion]
-    return {'emotion': dominant_emotion, 'score': score}
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = {"raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, json = myobj, headers = headers)
+    res = json.loads(response.text)
+    formatted_response = res['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(formatted_response, key = lambda x: formatted_response[x])
+    formatted_response['dominant_emotion'] = dominant_emotion
+    return formatted_response 
